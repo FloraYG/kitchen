b8472331f80f9b3c99b0b0ee75fc678e8a94a37e
floraYG <Flora.YG@hotmail.com>
2018-02-06 22:18:02 +0800

    Traverse folder.

---
+++ src/main/java/com/yg/kitchen/cookMp3/TransferMp3ToSony.java
@@ -0,0 +1,44 @@
+package com.yg.kitchen.cookMp3;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * 文件夹遍历
+ * Created by Ada.YG on 2018/2/6
+ */
+public class TransferMp3ToSony {
+    private String desPath = "H:\\MUSIC\\english";
+    private int count = 0;
+    public static void main(String[] args){
+        String sourcePath = "F:\\english";
+        File pFile = new File(sourcePath);
+        File[] files = pFile.listFiles();
+        TransferMp3ToSony transferMp3ToSony = new TransferMp3ToSony();
+        transferMp3ToSony.getFiles(files);
+        System.out.print("Finished.");
+    }
+    private void getFiles(File[] files){
+        Arrays.stream(files).forEach(file->{
+            if(file.isDirectory()){
+                getFiles(file.listFiles());
+            }else if(file.getName().contains("pb")){
+                try(OutputStream out = new FileOutputStream(Paths.get(desPath,file.getName()).toFile())) {
+                    Files.copy(Paths.get(file.getParent() + File.separator + file.getName()),out);
+                    System.out.print("The "+ count +" file copied");
+                    count++;
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
+        });
+    }
+}
+
+
+
